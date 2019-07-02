9eb6f21ab9b75d0678a3880824996ff3790566c3
kiugee <abhishekmazumdar118@gmail.com>
2019-07-02 15:05:18 +0530

    Login Check

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ router.get('/signup', function(req, res ){
 
 //Posting registration form
 router.post('/signup', function(req, res) { 
-    db.collection('userRegister').insert({name:req.body.name, email:req.body.email, username:req.body.username, password:req.body.password});
+    db.collection('userRegister').insertOne({name:req.body.name, email:req.body.email, username:req.body.username, password:req.body.password});
     res.render('signin',{title: 'Login', msg: 'Succefully registered. Now you may login.'});
     console.log(JSON.stringify(req.body) + " added to the db.userRegister"); 
 });
@@ -19,13 +19,20 @@ router.get('/signin', function(req, res ){
 });
 
 router.post('/signin',function(req,res){
-    db.collection('userRegister').findOne(req.body.username, function(err, result){
+    var User= {
+           user: req.body.username,
+           pass: req.body.password
+    }
+    
+    db.collection('userRegister').findOne(User, function(err, result){
+      
         if(err) throw err;
         else if(!result) {
             res.render('signin', {error: 'Not a valid username.'})
         }else{
-            if(req.body.password === result.password) {
+            if(result) {
                 req.session.loggedIn = true; 
+                console.log("Success")
                 res.render('profile', {msg: "Welcome"})   // allow the user
             }else{
                 res.render('signin', {error: "Invalid credentials"})
@@ -35,7 +42,14 @@ router.post('/signin',function(req,res){
     })
     
 });
-    
+
+//Profile routings
+
+router.get('/profile', function(req, res){
+    if(req.session.loggedIn===true) {
+        res.render('profile', {title: 'Profile'})
+    }  res.render('profile', {title: 'Profile'})
+}) 
 
 
 module.exports = router;
